ca9aa86761b943a60dd82a32e16f8d26d536fc3f
Canberk Yilmaz <canberk.yilmaz439@gmailcom>
2022-07-01 01:07:36 -0400

    Initial Commit

---
+++ FinalProject2_Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject2_Login
+//
+//  Created by canberk yılmaz on 2022-07-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FinalProject2_LoginUITests/FinalProject2_LoginUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FinalProject2_LoginUITestsLaunchTests.swift
+//  FinalProject2_LoginUITests
+//
+//  Created by canberk yılmaz on 2022-07-01.
+//
+
+import XCTest
+
+class FinalProject2_LoginUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
